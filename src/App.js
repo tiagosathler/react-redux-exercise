d41fa74f77e3a6364b16e7c3dffb573720b37a77
tiagosathler <sathler@gmail.com>
2021-10-10 19:19:59 -0300

    Insere o BrowserRoute e Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">Home</Route>
+          <Route path="/login">Login</Route>
+          <Route path="/cadastrar">Cadastrar Cliente</Route>
+          <Route path="/cadastros">Clientes Cadastrados</Route>
+          <Route>Not found</Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
