e087e172cc12661605ffbeab841ad07ddbc8778d
tiagosathler <sathler@gmail.com>
2021-10-11 07:52:24 -0300

    Branch 'solucao-tiago-sathler'

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// SOLUCAO DE TIAGO-SATHLER FEITA A PARTIR DA BRANCH tiago-sathler, MERGEADA
+// A BRANCH master E COPIADA PARA ESTA BRANCH solucao-tiago-sathler
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
